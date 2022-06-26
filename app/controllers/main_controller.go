bb69a73abb08786113225420993514fe4b066584
missiona.carla <missiona.carla@gmail.com>
2022-06-26 09:39:50 +0000

    Struct to json

--- app/controllers/main_controller.go
+++ app/controllers/main_controller.go
@@ -1,56 +1,148 @@
 package controllers
 
 import (
-   "loremipsumbytes/pkg/hasura"
-  "net/http"
-  "github.com/gofiber/fiber/v2"
-  "io/ioutil"
-  "log"
-  "encoding/json"
-  "time"
-  "fmt"
-  "bytes" 
-  )
+    "loremipsumbytes/pkg/hasura"
+    "net/http"
+    "github.com/gofiber/fiber/v2"
+    "io/ioutil"
+    "log"
+    "encoding/json"
+    "time"
+    "fmt"
+    "bytes"
+    "github.com/davecgh/go-spew/spew"
+)
+ 
 
+type User struct {
+    Full_name string
+    Email string
+}
 
+type Response struct {
+    Msg    string
+    User   interface{}
+    Status int
+    Data       []Users
+}
+// var post Article 
+//   json.Unmarshal(reqBody, &post)
 var Url = "https://loremdb.hasura.app/v1/graphql"
 
-func RenderIndex(c *fiber.Ctx) error {
- //*********/ Query Users 
-  q := hasura.Query_user()
-
-  req , err := http.NewRequest("POST", Url, bytes.NewBuffer(q)) 
-  if err != nil {
-    log.Println("Error in Gql Run")
-    
-  }
-  client := &http.Client{Timeout: time.Second * 7}
-  response, err := client.Do(req)
-  defer response.Body.Close()
-   if err != nil {
-    log.Println("Error in Clieee snt Do Run")
+func RenderIndex(c * fiber.Ctx) error {
+    //*********/ Query Users 
+    q:= hasura.Query_user()
+    req, err:= http.NewRequest("POST", Url, bytes.NewBuffer(q))
+    if err != nil {
+        log.Println("Error in Gql Run") }
+  
+    client:= & http.Client {
+        Timeout: time.Second * 7
     }
-  data, _ := ioutil.ReadAll(response.Body)
-  log.Println(string(data)) ; log.Println("%#v\n, data");   
-    log.Println("%#v\n, data") ;   log.Println("%#v\n, data");   log.Println("%#v\n, data")
-   fmt.Printf("======%#v\n", response)
- 
-  var jrsp map[string]interface{} = nil
+    response, err:= client.Do(req)
+    defer response.Body.Close()
+    if err != nil {
+        log.Println("Error in Clieee snt Do Run")
+    }
+    data, _:= ioutil.ReadAll(response.Body)
+     log.Println("string(data)");  log.Println("string(data)");  log.Println("string(data)");
 
-  if err := json.Unmarshal(data, &jrsp); err != nil {  
+    log.Println(string(data));
+    log.Println("%#v\n, data");
+    log.Println("%#v\n, data");log.Println("%#v\n, data");log.Println("%#v\n, data")
+    fmt.Printf("======%#v\n", response.Body)
+
+    var jrsp map[string]interface {} = nil
+
+    if err:= json.Unmarshal(data, & jrsp);err != nil {
         log.Println("Can not unmnarshal JSON")
     }
-  log.Printf("",response) 
-  log.Println("Can  unmarshal JSON")
-   log.Println("!an  unmarshal JSON")
-     log.Println("!!!!!Can  unmarshal JSON")
-     log.Println("Can  unmarshal JSON")
-   log.Println(jrsp) 
-  
-json.NewDecoder(response.Body).Decode(jrsp)
-log.Println("rense struct:", response)  
-	return c.Render("index", fiber.Map{
-		"FiberTitle": "Hello From Fiber Html Engine",
-	},"layouts/htm")
-}     
-  
+    log.Printf("", response)
+
+    log.Println(jrsp)
+    fmt.Printf("---\ngo-spew-----------\n")
+    spew.Dump(jrsp)
+    json.NewDecoder(response.Body).Decode(jrsp)
+    log.Println("rense structaaa:", jrsp)
+    return c.Render("index", fiber.Map {
+        "FiberTitle": "Hello From Fiber Html Engine",
+    }, "layouts/htm")
+}
+ 
+// type Response struct {
+//     Data       []Users
+   
+// }
+
+// type Users struct {
+//     Data string      `json:"data"`
+//     Key string            `json:"key"`
+//     Value string          `json:"value"`
+// }
+
+// func main() {
+//     s := string(`{"operation": "get", "key": "example"}`)
+//     data := Request{}
+//     json.Unmarshal([]byte(s), &data)
+//     fmt.Printf("Operation: %s", data.Operation)
+// }
+// /////////
+
+// type Request struct {
+//     Operation string      `json:"operation"`
+//     Key string            `json:"key"`
+//     Value string          `json:"value"`
+// }
+
+// func main() {
+//     s := string(`{"operation": "get", "key": "example"}`)
+//     data := Request{}
+//     json.Unmarshal([]byte(s), &data)
+//     fmt.Printf("Operation: %s", data.Operation)
+// }
+
+// ..
+// import (
+//   "encoding/json"
+//   "fmt"
+//   "io/ioutil"
+//   "log"
+//   "net/http"
+
+//   "github.com/gorilla/mux"
+// )
+
+// type Article struct {
+//   Id string `json:"Id"`
+//   Title string `json:"Title"`
+//   Content string `json:"Content"`
+//   Summary string `json:"Summary"`
+// }
+
+// ...
+
+// func createNewArticle(w http.ResponseWriter, r *http.Request) {
+//   reqBody, _ := ioutil.ReadAll(r.Body)
+//   var post Article 
+//   json.Unmarshal(reqBody, &post)
+
+//   json.NewEncoder(w).Encode(post)
+
+//   newData, err := json.Marshal(post)
+//   if err != nil {
+//     fmt.Println(err)
+//   } else {
+//     fmt.Println(string(newData))
+//   }
+// }
+
+// func handleReqs() {
+//   r := mux.NewRouter().StrictSlash(true)
+//   r.HandleFunc("/post", createNewArticle).Methods("POST")
+
+//   log.Fatal(http.ListenAndServe(":8000", r))
+// }
+
+// func main() {
+//   handleReqs();
+// }
--- go.mod
+++ go.mod
@@ -7,13 +7,9 @@ require (
 	github.com/go-playground/validator/v10 v10.11.0
 	github.com/gofiber/fiber/v2 v2.34.1
 	github.com/gofiber/template v1.6.28
-	github.com/google/go-cmp v0.5.7 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/hasura/go-graphql-client v0.7.1
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.6
 	github.com/stretchr/testify v1.7.1 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 	gorm.io/gorm v1.23.6
 
 )
